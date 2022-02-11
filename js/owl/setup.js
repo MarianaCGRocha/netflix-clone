51d1dd6f4d0cdee2764b4cd9d564d71e8caa4d51
MarianaCGRocha <marianagomesr@gmail.com>
2022-02-11 00:59:20 -0300

    Adição de todas alterações projeto clone netflix - Take Blip Bootcamp

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel-videos').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        480:{
+            items:2
+        },
+        600:{
+            items:4
+        }
+    }
+})
